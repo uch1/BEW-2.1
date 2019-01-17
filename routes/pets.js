0787060b925a206946cce63e2a3261a8d70995c8
Uchenna Aguocha <uk.aguocha@gmail.com>
2019-01-17 14:48:59 -0800

    Submit form via ajax with axios

--- routes/pets.js
+++ routes/pets.js
@@ -24,10 +24,12 @@ module.exports = (app) => {
 
     pet.save()
       .then((pet) => {
-        res.redirect(`/pets/${pet._id}`);
+        // res.redirect(`/pets/${pet._id}`);
+        res.send({ pet: pet });
       })
       .catch((err) => {
         // Handle Errors
+        res.status(400).send(err.errors);
       });
   });
 
