fd6abc545b858231ce84f6df635048015187e5db
Uchenna Aguocha <uk.aguocha@gmail.com>
2019-01-18 05:48:57 -0800

    Adding middleware for AWS

--- routes/pets.js
+++ routes/pets.js
@@ -1,6 +1,34 @@
 // MODELS
 const Pet = require('../models/pet');
 
+// UPLOADING TO AWS S3
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const Upload = require('s3-uploader');
+
+const client = new Upload(process.env.S3_BUCKET, {
+  aws: {
+    path: 'pets/avatar',
+    region: process.env.S3_REGION,
+    acl: 'public-read',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  },
+  cleanup: {
+    versions: true,
+    original: true
+  },
+  versions: [{
+    maxWidth: 400,
+    aspect: '16:10',
+    suffix: '-standard'
+  }, {
+    maxWidth: 300,
+    aspect: '1:1',
+    suffix: '-square'
+  }]
+});
+
 // PET ROUTES
 module.exports = (app) => {
 
